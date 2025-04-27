5840d54e49d296ac6e0ec9ec929ca0b9de754eb3
zoren <zoren@leit.so>
2025-04-27 14:39:06 -0700

    Completely redid the entity system with optional sprites and indexes no more circle:(

--- src/space-invaders.py
+++ src/space-invaders.py
@@ -1,50 +1,63 @@
 from pyray import *
 from random import randint, choice
+from os.path import join as os
+
+# Defining variables
+debug = False
 
 # General entity class 
 class Entity:
     alive = 1
 
-    def __init__(self, x, y, size, color):
-        self.x = x
-        self.y = y
-        self.size = size
+    def __init__(self, sprite=None, index=None, rectangle=Rectangle, color=None):
+        self.sprite = sprite
+        self.index = index
+        self.rectangle = rectangle
         self.color = color
 
 # Defining player 
-player = Rectangle(45,90,10,10)
+player_pos = Vector2(42,84)
+player_sprite = os('assets','space_invaders_player.png')
 
 # Defining lists for drawing entities
 enemies = []
+enemy_sprite = os('assets','space_invaders_enemies.png')
 bullets = []
 
 # Initiating window
 init_window(100, 100, "raylib spaceinvaders")
 set_target_fps(60)
 
+# Loading textures
+player_sprite = load_texture(player_sprite)
+enemy_sprite = load_texture(enemy_sprite)
+
 # Defining functions for entities
 def summon_entity(entity, entity_list: []):
     entity_list.append(entity)   
-def draw_entity(entity):
-    draw_circle(entity.x,entity.y,entity.size,entity.color)
     
 while not window_should_close():
 
     # Defining entities
-    bullet = Entity(int(player.x+5),int(player.y),3,RED)
-    enemy = Entity(randint(6,94),6,5,BLUE)
+    bullet = Entity(None ,None, Rectangle(int(player_pos.x+8),int(player_pos.y), 3, 3), RED)
+    enemy = Entity(enemy_sprite, None, Rectangle(0,0,32,32),WHITE)
 
     begin_drawing()
     clear_background(BLACK)
 
     # Drawing player
-    draw_rectangle_rec(player, WHITE)
-
+    draw_texture_ex(player_sprite,player_pos,0,.5,WHITE)
     # Movement
-    if is_key_down(KeyboardKey.KEY_A) and player.x > 0:
-        player.x -= 1
-    if is_key_down(KeyboardKey.KEY_D) and player.x < 90:
-        player.x += 1
+    if is_key_down(KEY_A) and player_pos.x > 0:
+        player_pos.x -= 1
+    if is_key_down(KEY_D) and player_pos.x < 84:
+        player_pos.x += 1
+    if is_key_down(KEY_UP) and player_pos.y > -3:
+        player_pos.y -= 1
+    if is_key_down(KEY_DOWN) and player_pos.y < 84:
+        player_pos.y += 1
+    if is_key_pressed(KEY_F3):
+        debug = not debug
 
     # Combat
     if is_key_pressed(KeyboardKey.KEY_SPACE):
@@ -55,34 +68,24 @@ while not window_should_close():
 
     # Draw entities
     for i in enemies:
-        draw_entity(i)
+        draw_texture(i.sprite, int(i.rectangle.x), int(i.rectangle.y), i.color)
 
     for i in bullets:
-        draw_entity(i)
-        i.y -= 1
+        draw_rectangle(int(i.rectangle.x),int(i.rectangle.y),int(i.rectangle.width),int(i.rectangle.height),i.color)
+        i.rectangle.y -= 1
 
     # Removing dead entities and useen entities
-    for obj in enemies[:]:
-        if obj.alive != 1:
-            del enemies[enemies.index(obj)]
+    # for obj in enemies[:]:
+    #     if obj.alive != 1:
+    #         del enemies[enemies.index(obj)]
 
-    for obj in bullets[:]:
-        if obj.alive != 1 or obj.y < -2:
-            del bullets[bullets.index(obj)]
-
-
-    # Detecting colision between a bullet and a enemy
-    for b in bullets:
-        for e in enemies:
-            dist_x = abs(b.x - e.x)
-            dist_y = abs(b.y - e.y)
-            dist = dist_x + dist_y
-            if dist < 3 + e.size:
-                b.alive = 0
-                e.alive = 0
+    # for obj in bullets[:]:
+    #     if obj.alive != 1 or obj.y < -2:
+    #         del bullets[bullets.index(obj)]
 
     # DEBUG
-    draw_text(f'bullets:{len(bullets)}\nenemies:{len(enemies)}',0,0,5,GREEN)
+    if debug:
+        draw_text(f'bullets:{len(bullets)}\nenemies:{len(enemies)}\nx:{player_pos.x} y:{player_pos.y}',0,0,5,GREEN)
 
     end_drawing()
 
